fca74926a9c61a296f8036ed3db10f9b08cc40ab
Rocco Manzo <manzor99@gmail.com>
2018-03-29 20:07:10 -0400

    made a Dino class and made a method to find the top 5 dinos in the population

--- src/genetics.js
+++ src/genetics.js
@@ -5,21 +5,55 @@ Rocco Manzo - Rmanzo
 
 var population = []; //the population of neural networks (should have around 20)
 var MAX_DINOS = 20;
+
 //creates the initial Population
-createPop : function() = {
+makePopulation : function() = {
   this.population.splice(0, this.population.length);
 
   for(var i = 0; i < MAX_DINOS; i++){
-    var newDino = new synpatic.Architect.Perceptron(0,0,0);//change????
-
-    newDino.index = i;
-    newDino.fitness = 0;
-    newDino.score = 0;
-    newDino.isWinner = false;
-
+    var newDino = new Dino(0, new synpatic.Architect.Perceptron(0,0,0), i);
     this.population.push(newDino);
   }
 
+}
+
+//returns an array of the top 5 Dinos
+getWinners : function() = {
+  var winners = []; //list of winners
+
+  //add the first 5 dinos from the pop to the winners
+  for(int i = 0; i < 5; i++){
+    winners.push(population[i]);
+  }
+
+  //sorting the list so the highest fitness dino is in position 0
+  //ik insertion sort is slow but it's 5 long so it's fine
+  for(int i = 1; i < winners.length; i++){
+    for(int j = i; j > 0; j--){
+      if(winners[j].fitness <= winners[j-1].fitness){
+        break;
+      }
+      else{
+        var temp = winners[i];
+        winners[i] = winners[i-1];
+        winners[i-1] = temp;
+      }
+    }
+  }
+
+  //loop through the rest of the population to find the top 5
+  //start looping at 5 because 0-4 are already in the winners array
+  for(int i = 5; i < population.length; i++){
+      for(int j = winners.length-1; j >= 0; j--){
+        if(population[i].fitness > winners[j].fitness){
+          winners[j] = population[i];
+        }
+        else{
+          break;
+        }
+      }
+  }
+  return winners;
 }
 
 //evolves the population
